50e998f78b416a9bea913370816b63f51ce2ae2c
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-06-27 10:13:14 +0400

    v0.0.3

--- src/custom-promise/rejectAsResolve.ts
+++ src/custom-promise/rejectAsResolve.ts
@@ -1,4 +1,4 @@
-function promiseRejected(reason?: any): PromiseLike<never> {
+function _promiseRejected(reason?: any): PromiseLike<never> {
   return {
     then(_, reject): any {
       void reject(reason)
@@ -6,6 +6,18 @@ function promiseRejected(reason?: any): PromiseLike<never> {
   }
 }
 
+/**
+ * It needed to prevent memory leak on some platforms
+ * see: https://stackoverflow.com/questions/72727907/promise-reject-is-very-slow-on-node-js-18
+ *  */
 export function rejectAsResolve(resolve: (value: any) => void, reason?: any) {
-  resolve(promiseRejected(reason))
+  resolve(_promiseRejected(reason))
+}
+
+/**
+ * It needed to prevent memory leak on some platforms
+ * see: https://stackoverflow.com/questions/72727907/promise-reject-is-very-slow-on-node-js-18
+ * */
+export function promiseRejected(reason?: any): Promise<never> {
+  return Promise.resolve(_promiseRejected(reason))
 }
