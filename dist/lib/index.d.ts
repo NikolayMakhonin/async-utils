13f20ddcb67c31c7c64629a7ee87831495267f74
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-07-02 17:11:15 +0400

    develop: added PromiseOrValue

---
+++ dist/lib/index.d.ts
@@ -0,0 +1,5 @@
+export * from './delay';
+export * from './custom-promise';
+export * from './abort-controller-fast';
+export * from './custom-promise/rejectAsResolve';
+export * from './isPromiseLike';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from './types'
 export * from './delay'
 export * from './custom-promise'
 export * from './abort-controller-fast'
