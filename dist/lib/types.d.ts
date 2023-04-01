5b6d736f21b4e77b535ac6dc7decd6a31ee37607
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2023-04-01 08:41:21 +0400

    v0.0.19 added ValueState

--- dist/lib/types.d.ts
+++ dist/lib/types.d.ts
@@ -1,1 +1,5 @@
 export declare type PromiseOrValue<T> = Promise<T> | T;
+export declare type OfPromise<T> = T extends Promise<infer U> ? U : T;
+export declare type ToPromise<T> = Promise<T extends Promise<infer U> ? U : T>;
+export declare type Func<This, Args extends any[], Result> = (this: This, ...args: Args) => Result;
+export declare type FuncAny = Func<any, any[], any>;
