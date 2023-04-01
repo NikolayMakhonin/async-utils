b9f34449e0ccdb0cc8bd87b042a80ec43cef305c
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2023-04-01 08:40:01 +0400

    v0.0.19 added ValueState

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,10 @@
 export type PromiseOrValue<T> = Promise<T> | T
+
+export type OfPromise<T> = T extends Promise<infer U> ? U : T
+
+export type ToPromise<T> = Promise<T extends Promise<infer U> ? U : T>
+
+export type Func<This, Args extends any[], Result>
+  = (this: This, ...args: Args) => Result
+
+export type FuncAny = Func<any, any[], any>
